83e934c10071af597857af8c5f6aaa433b88fa17
gian-byte <85333548+gian-byte@users.noreply.github.com>
2021-07-03 08:19:57 +0800

    Add files via upload

---
+++ FailedTicTacToe.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace TicTacToe
+{
+    partial class FailedTicTacToe
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.BottomRight = new System.Windows.Forms.Button();
+            this.MiddleRight = new System.Windows.Forms.Button();
+            this.UpperRight = new System.Windows.Forms.Button();
+            this.LowerMiddle = new System.Windows.Forms.Button();
+            this.Middle = new System.Windows.Forms.Button();
+            this.UpperMiddle = new System.Windows.Forms.Button();
+            this.BottomLeft = new System.Windows.Forms.Button();
+            this.LeftMid = new System.Windows.Forms.Button();
+            this.TopLeft = new System.Windows.Forms.Button();
+            this.Congratulations = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.LavenderBlush;
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox1.Controls.Add(this.BottomRight);
+            this.groupBox1.Controls.Add(this.MiddleRight);
+            this.groupBox1.Controls.Add(this.UpperRight);
+            this.groupBox1.Controls.Add(this.LowerMiddle);
+            this.groupBox1.Controls.Add(this.Middle);
+            this.groupBox1.Controls.Add(this.UpperMiddle);
+            this.groupBox1.Controls.Add(this.BottomLeft);
+            this.groupBox1.Controls.Add(this.LeftMid);
+            this.groupBox1.Controls.Add(this.TopLeft);
+            this.groupBox1.ForeColor = System.Drawing.Color.DarkRed;
+            this.groupBox1.Location = new System.Drawing.Point(25, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(330, 393);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // BottomRight
+            // 
+            this.BottomRight.BackColor = System.Drawing.Color.Aquamarine;
+            this.BottomRight.Font = new System.Drawing.Font("Segoe UI", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BottomRight.Location = new System.Drawing.Point(216, 253);
+            this.BottomRight.Name = "BottomRight";
+            this.BottomRight.Size = new System.Drawing.Size(92, 105);
+            this.BottomRight.TabIndex = 8;
+            this.BottomRight.UseVisualStyleBackColor = false;
+            this.BottomRight.Click += new System.EventHandler(this.BRButtonClick);
+            // 
+            // MiddleRight
+            // 
+            this.MiddleRight.BackColor = System.Drawing.Color.Aquamarine;
+            this.MiddleRight.Font = new System.Drawing.Font("Segoe UI", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.MiddleRight.Location = new System.Drawing.Point(216, 142);
+            this.MiddleRight.Name = "MiddleRight";
+            this.MiddleRight.Size = new System.Drawing.Size(92, 105);
+            this.MiddleRight.TabIndex = 7;
+            this.MiddleRight.UseVisualStyleBackColor = false;
+            this.MiddleRight.Click += new System.EventHandler(this.MRButtonClick);
+            // 
+            // UpperRight
+            // 
+            this.UpperRight.BackColor = System.Drawing.Color.Aquamarine;
+            this.UpperRight.Font = new System.Drawing.Font("Segoe UI", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.UpperRight.Location = new System.Drawing.Point(216, 31);
+            this.UpperRight.Name = "UpperRight";
+            this.UpperRight.Size = new System.Drawing.Size(92, 105);
+            this.UpperRight.TabIndex = 6;
+            this.UpperRight.UseVisualStyleBackColor = false;
+            this.UpperRight.Click += new System.EventHandler(this.URButtonClick);
+            // 
+            // LowerMiddle
+            // 
+            this.LowerMiddle.BackColor = System.Drawing.Color.Aquamarine;
+            this.LowerMiddle.Font = new System.Drawing.Font("Segoe UI", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LowerMiddle.Location = new System.Drawing.Point(118, 253);
+            this.LowerMiddle.Name = "LowerMiddle";
+            this.LowerMiddle.Size = new System.Drawing.Size(92, 105);
+            this.LowerMiddle.TabIndex = 5;
+            this.LowerMiddle.UseVisualStyleBackColor = false;
+            this.LowerMiddle.Click += new System.EventHandler(this.LMButtonClick);
+            // 
+            // Middle
+            // 
+            this.Middle.BackColor = System.Drawing.Color.Aquamarine;
+            this.Middle.Font = new System.Drawing.Font("Segoe UI", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Middle.Location = new System.Drawing.Point(118, 142);
+            this.Middle.Name = "Middle";
+            this.Middle.Size = new System.Drawing.Size(92, 105);
+            this.Middle.TabIndex = 4;
+            this.Middle.UseVisualStyleBackColor = false;
+            this.Middle.Click += new System.EventHandler(this.MButtonClick);
+            // 
+            // UpperMiddle
+            // 
+            this.UpperMiddle.BackColor = System.Drawing.Color.Aquamarine;
+            this.UpperMiddle.Font = new System.Drawing.Font("Segoe UI", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.UpperMiddle.Location = new System.Drawing.Point(118, 31);
+            this.UpperMiddle.Name = "UpperMiddle";
+            this.UpperMiddle.Size = new System.Drawing.Size(92, 105);
+            this.UpperMiddle.TabIndex = 3;
+            this.UpperMiddle.UseVisualStyleBackColor = false;
+            this.UpperMiddle.Click += new System.EventHandler(this.UMButtonClick);
+            // 
+            // BottomLeft
+            // 
+            this.BottomLeft.BackColor = System.Drawing.Color.Aquamarine;
+            this.BottomLeft.Font = new System.Drawing.Font("Segoe UI", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BottomLeft.Location = new System.Drawing.Point(20, 253);
+            this.BottomLeft.Name = "BottomLeft";
+            this.BottomLeft.Size = new System.Drawing.Size(92, 105);
+            this.BottomLeft.TabIndex = 2;
+            this.BottomLeft.UseVisualStyleBackColor = false;
+            this.BottomLeft.Click += new System.EventHandler(this.BLButtonClick);
+            // 
+            // LeftMid
+            // 
+            this.LeftMid.BackColor = System.Drawing.Color.Aquamarine;
+            this.LeftMid.Font = new System.Drawing.Font("Segoe UI", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LeftMid.Location = new System.Drawing.Point(20, 142);
+            this.LeftMid.Name = "LeftMid";
+            this.LeftMid.Size = new System.Drawing.Size(92, 105);
+            this.LeftMid.TabIndex = 1;
+            this.LeftMid.UseVisualStyleBackColor = false;
+            this.LeftMid.Click += new System.EventHandler(this.MLButtonClick);
+            // 
+            // TopLeft
+            // 
+            this.TopLeft.BackColor = System.Drawing.Color.Aquamarine;
+            this.TopLeft.Font = new System.Drawing.Font("Segoe UI", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TopLeft.Location = new System.Drawing.Point(20, 31);
+            this.TopLeft.Name = "TopLeft";
+            this.TopLeft.Size = new System.Drawing.Size(92, 105);
+            this.TopLeft.TabIndex = 0;
+            this.TopLeft.UseVisualStyleBackColor = false;
+            this.TopLeft.Click += new System.EventHandler(this.ULButtonClick);
+            // 
+            // Congratulations
+            // 
+            this.Congratulations.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Congratulations.Location = new System.Drawing.Point(392, 37);
+            this.Congratulations.Name = "Congratulations";
+            this.Congratulations.Size = new System.Drawing.Size(386, 23);
+            this.Congratulations.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 450);
+            this.Controls.Add(this.Congratulations);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button MiddleRight;
+        private System.Windows.Forms.Button UpperRight;
+        private System.Windows.Forms.Button LowerMiddle;
+        private System.Windows.Forms.Button Middle;
+        private System.Windows.Forms.Button UpperMiddle;
+        private System.Windows.Forms.Button BottomLeft;
+        private System.Windows.Forms.Button LeftMid;
+        private System.Windows.Forms.Button TopLeft;
+        private System.Windows.Forms.Button BottomRight;
+        private System.Windows.Forms.TextBox Congratulations;
+    }
+}
+
---
+++ FailedTicTacToe.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class FailedTicTacToe : Form
+    {
+        public FailedTicTacToe()
+        {
+            InitializeComponent();
+        }
+        bool player;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        private void UMButtonClick(object sender, EventArgs e)
+        {
+            if (player == false)
+                UpperMiddle.Text = "X";
+            if (player == true)
+                UpperMiddle.Text = "O";
+            UpperMiddle.Enabled = false;
+            if (player == false)
+                player = true;
+            else
+                player = false;
+        }
+        private void URButtonClick(object sender, EventArgs e)
+        {
+            if (player == false)
+                UpperRight.Text = "X";
+            else
+                UpperRight.Text = "O";
+            UpperRight.Enabled = false;
+            if (player == false)
+                player = true;
+            else
+                player = false;
+        }
+        private void MLButtonClick(object sender, EventArgs e)
+        {
+            if (player == false)
+                LeftMid.Text = "X";
+            else
+                LeftMid.Text = "O";
+            LeftMid.Enabled = false;
+            if (player == false)
+                player = true;
+            else
+                player = false;
+        }
+        private void MButtonClick(object sender, EventArgs e)
+        {
+            if (player == false)
+                Middle.Text = "X";
+            else
+                Middle.Text = "O";
+            Middle.Enabled = false;
+            if (player == false)
+                player = true;
+            else
+                player = false;
+        }
+        private void MRButtonClick(object sender, EventArgs e)
+        {
+            if (player == false)
+                MiddleRight.Text = "X";
+            else
+                MiddleRight.Text = "O";
+            MiddleRight.Enabled = false;
+            if (player == false)
+                player = true;
+            else
+                player = false;
+        }
+        private void BLButtonClick(object sender, EventArgs e)
+        {
+            if (player == false)
+                BottomLeft.Text = "X";
+            else
+                BottomLeft.Text = "O";
+            BottomLeft.Enabled = false;
+            if (player == false)
+                player = true;
+            else
+                player = false;
+        }
+        private void LMButtonClick(object sender, EventArgs e)
+        {
+            if (player == false)
+                LowerMiddle.Text = "X";
+            else
+                LowerMiddle.Text = "O";
+            LowerMiddle.Enabled = false;
+            if (player == false)
+                player = true;
+            else
+                player = false;
+        }
+        private void BRButtonClick(object sender, EventArgs e)
+        {
+            if (player == false)
+                BottomRight.Text = "X";
+            else
+                BottomRight.Text = "O";
+            BottomRight.Enabled = false;
+            if (player == false)
+                player = true;
+            else
+                player = false;
+        }
+        private void ULButtonClick(object sender, EventArgs e)
+        {
+            if (player == false)
+                TopLeft.Text = "X";
+            else
+                TopLeft.Text = "O";
+            TopLeft.Enabled = false;
+            if (player == false)
+                player = true;
+            else
+                player = false;
+        }
+        public void Winner()
+        {
+           bool wincondition = false;
+            if (wincondition == true)
+                Congratulations.Text = "Congratulations!";
+            if ((TopLeft.Text == UpperMiddle.Text) && (UpperMiddle.Text == UpperRight.Text))
+                wincondition = true;
+            if ((LeftMid.Text == Middle.Text) && (Middle.Text == MiddleRight.Text))
+                wincondition = true;
+            if ((BottomLeft.Text == LowerMiddle.Text) && (LowerMiddle.Text == BottomRight.Text))
+                wincondition = true;
+            if ((TopLeft.Text == LeftMid.Text) && (LeftMid.Text == BottomLeft.Text))
+                wincondition = true;
+            if ((UpperMiddle.Text == Middle.Text) && (Middle.Text == LowerMiddle.Text))
+                wincondition = true;
+            if ((UpperRight.Text == MiddleRight.Text) && (MiddleRight.Text == BottomRight.Text))
+                wincondition = true;
+            if ((UpperRight.Text == Middle.Text) && (Middle.Text == BottomLeft.Text))
+                wincondition = true;
+            if ((TopLeft.Text == Middle.Text) && (Middle.Text == BottomRight.Text))
+                wincondition = true;
+        }
+    }
+}
